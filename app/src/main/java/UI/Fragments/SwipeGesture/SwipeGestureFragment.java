810ea04e71090a8153fa6e00a75a1e25e9884b47
vmoysiadis <v.s.moisiadis@gmail.com>
2020-03-22 20:21:08 +0200

    SwipeFragment: Remove unused code

--- app/src/main/java/UI/Fragments/SwipeGesture/SwipeGestureFragment.java
+++ app/src/main/java/UI/Fragments/SwipeGesture/SwipeGestureFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,24 +16,13 @@ import android.view.ViewGroup;
 import com.base.gesturer.R;
 
 public class SwipeGestureFragment extends Fragment {
-
-    private SwipeGestureViewModel mViewModel;
-
-    public static SwipeGestureFragment newInstance() {
-        return new SwipeGestureFragment();
-    }
-
+    
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.swipe_gesture_fragment, container, false);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SwipeGestureViewModel.class);
-        // TODO: Use the ViewModel
-    }
+
 
 }
